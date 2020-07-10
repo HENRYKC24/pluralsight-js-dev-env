f8371d1a7f72a63404848846d20faf7a27f56755
HENRY-KC <henrykc24@yahoo.com>
2020-07-10 21:31:10 +0100

    feature: add mock api for http calls

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
-import './index.css'
-import numeral from 'numeral';
+import {getUsers, deleteUser} from './api/userApi';
 
-/* eslint-disable no-console */
+// Populate table of users via API call.
+getUsers().then(result => {
+  console.log(result + 'i did'); // eslint-disable-line no-console
+  let usersBody = '';
 
-const courseValue = numeral(1000).format('$0,0.00');
-console.log(`I would pay ${courseValue} for this awesome course!`);
+  result.forEach(user => {
+    usersBody += `<tr>
+      <td><a href='#' data-id='${user.id}' class="deleteUser">Delete</a></td>
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+    </tr>`;
+  });
+
+  global.document.getElementById('users').innerHTML = usersBody;
+
+  const deleteLink = global.document.getElementsByClassName('deleteUser');
+
+  // Must use array.from to create a real array from a DOM collection
+  // getElementsByClassName only returns an "array like" object
+  Array.from(deleteLink, link => {
+    link.onclick = function(event) {
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    };
+  });
+})
