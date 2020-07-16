b00622198e1a31ce59cef78beb2335244ba88d5c
HENRY-KC <henrykc24@yahoo.com>
2020-07-16 10:11:24 +0100

    update code comment

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
-  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+  return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://glacial-ocean-79839.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
